262f40bda0d7cb1dedec5856d0f805f90ebeb1c7
HaitianAsassin <exlab55@gmail.com>
2025-03-11 23:44:12 -0400

    Deploy to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 declare global {
@@ -9,13 +7,8 @@ declare global {
   }
 }
 
-const backgroundIMG =[
-  {
-    url: 'https://cdn.leonardo.ai/users/a482cc8c-f5e8-4326-8038-82c383032a5a/generations/ebe27154-771f-4505-9148-cdca44c52252/Leonardo_Phoenix_09_a_highly_detailed_expressive_and_vibrant_r_0.jpg?w=512',
-  }
-]
+
 function App() {
-  const [count, setCount] = useState(0)
   
   useEffect(() => {
     const script = document.createElement('script')
